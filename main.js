c1fa12ac89efd8658b2e3b7795377803bf1765dc
Donis <petrdonatas@gmail.com>
2016-04-16 01:49:36 +0300

    test

--- main.js
+++ main.js
@@ -10,10 +10,37 @@ app.get("/", function(req, res)
 app.use("/client", express.static(__dirname + "/client"));
 
 serv.listen(2000);
+console.log("server started");
+
+var SOCKET_LIST = {};
 
 
 var io = require("socket.io")(serv,{});
 io.sockets.on("connection", function(socket)
 {
-   console.log("socket connection"); 
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    SOCKET_LIST[socket.id] = socket;
 });
+
+setInterval(function()
+{
+    var pack = [];
+    for(var i in SOCKET_LIST)
+    {
+        var socket = SOCKET_LIST[i];
+        socket.x++;
+        socket.y++;
+        pack.push(
+        {
+            x:socket.x,
+            y:socket.y
+        });
+    }
+    
+    for(var i in SOCKET_LIST)
+    {
+        socket.emit("newPositions", pack);
+    }
+}, 1000/25);
